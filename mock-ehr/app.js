2adbdf1dc0530a07d6e925e6f550e250ca7f2689
VEF <victorevector@gmail.com>
2019-02-10 01:30:34 -0800

    mock message edits: match payload to mock scenario

--- mock-ehr/app.js
+++ mock-ehr/app.js
@@ -1,6 +1,6 @@
 // MOCK MESSAGE
-const msg = "MSH|^~\&|EPIC|EPICADT|SMS|SMSADT|199912271408|CHARRIS|ADT^A04|1817457|D|2.5<cr>PID||0493575^^^2^ID 1|454721||DOE^JOHN^^^^|DOE^JOHN^^^^|19480203|M||B|254 MYSTREET AVE^^MYTOWN^OH^44123^USA||(216)123-4567|||M|NON|400003403~1129086<cr>NK1||ROE^MARIE^^^^|SPO||(216)123-4567||EC||||||||||||||||||||||||||<cr>PV1||O|168 ~219~C~PMA^^^^^^^^^||||277^ALLEN MYLASTNAME^BONNIE^^^^|||||||||| ||2688684|||||||||||||||||||||||||199912271408||||||002376853<cr>"
-
+const fs = require('fs');
+const msg = fs.readFileSync('./lab.hl7', 'utf-8');
 
 // TCP CONNECTION
 const net = require('net');
@@ -36,5 +36,5 @@ server.on('listening', () => {
     setInterval(() => {
         var c = new tcpClient(remotePort, remoteHost);
         c.sendOne(msg);
-    }, 3000);
+    }, 10000);
 });
